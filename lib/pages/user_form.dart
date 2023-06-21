6b0cb8555843ab382b13c5c417b89dd665e0ee09
mredaoudi <daoudi.mreda@outlook.com>
2023-06-21 11:31:47 +0200

    version: 1.0.1+2

--- lib/pages/user_form.dart
+++ lib/pages/user_form.dart
@@ -22,70 +22,81 @@ class UserFormState extends State<UserForm> {
   Widget build(BuildContext context) {
     final peopleProvider = Provider.of<PeopleProvider>(context);
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(100, 100, 100, 50),
-              child: Image.asset('assets/icon/icon.png'),
-            ),
-            const Padding(
-              padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
-              child: Text(
-                "بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ",
-                style: TextStyle(fontSize: 20),
-                textAlign: TextAlign.center,
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(100, 100, 100, 50),
+                child: Image.asset('assets/icon/icon.png'),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 16),
-              child: TextFormField(
-                inputFormatters: [
-                  FilteringTextInputFormatter.allow(RegExp("[a-zA-Z]")),
-                ],
-                decoration: const InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'Enter Your Name',
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    text = value;
-                  });
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.resolveWith<Color>((states) {
-                    if (states.contains(MaterialState.disabled)) {
-                      return const Color(0xFFD6D6D6); // Disabled color
-                    }
-                    return const Color(0xFF4E6E81); // Regular color
-                  }),
-                  minimumSize: const MaterialStatePropertyAll(Size(50, 50)),
-                ),
-                onPressed: text == ''
-                    ? null
-                    : () async {
-                        peopleProvider.createPerson(
-                          name: serverName(text),
-                          firstTime: true,
-                        );
-                        Navigator.pushNamed(context, '/');
-                      },
-                child: const Text(
-                  "Add person",
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                child: Text(
+                  "بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ",
                   style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
                 ),
               ),
-            ),
-          ],
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                child: Text(
+                  "بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ",
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+                child: TextFormField(
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(RegExp("[a-zA-Z]")),
+                  ],
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Enter Your Name',
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      text = value;
+                    });
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.resolveWith<Color>((states) {
+                      if (states.contains(MaterialState.disabled)) {
+                        return const Color(0xFFD6D6D6); // Disabled color
+                      }
+                      return const Color(0xFF4E6E81); // Regular color
+                    }),
+                    minimumSize: const MaterialStatePropertyAll(Size(50, 50)),
+                  ),
+                  onPressed: text == ''
+                      ? null
+                      : () async {
+                          peopleProvider.createPerson(
+                            name: serverName(text),
+                            firstTime: true,
+                          );
+                          Navigator.pushNamed(context, '/');
+                        },
+                  child: const Text(
+                    "Add person",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
